3bb591b385f4b8a33f38511bef198cab390a2aad
Moritz Schillinger <Moritz.Schillinger@ffuf.de>
2017-06-15 20:53:18 +0200

    Initial Commit

---
+++ DemoCV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoCV
+//
+//  Created by Moritz Schillinger on 15.06.17.
+//  Copyright © 2017 Moritz Schillinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
