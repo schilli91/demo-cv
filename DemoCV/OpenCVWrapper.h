226b6ac830df804b4a350381b97ba7cf13b2dc02
Moritz Schillinger <Moritz.Schillinger@ffuf.de>
2017-06-15 21:58:06 +0200

    successfully setup OpenCV, though the actual framework is not pushed to repo

---
+++ DemoCV/OpenCVWrapper.h
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.h
+//  DemoCV
+//
+//  Created by Moritz Schillinger on 15.06.17.
+//  Copyright © 2017 Moritz Schillinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCVWrapper : NSObject
+- (void)isThisWorking;
+@end
---
+++ DemoCV/OpenCVWrapper.mm
@@ -0,0 +1,18 @@
+//
+//  OpenCVWrapper.m
+//  DemoCV
+//
+//  Created by Moritz Schillinger on 15.06.17.
+//  Copyright © 2017 Moritz Schillinger. All rights reserved.
+//
+
+#import <opencv2/opencv.hpp>
+#import "OpenCVWrapper.h"
+
+using namespace std;
+
+@implementation OpenCVWrapper
+- (void) isThisWorking {
+    cout << "Hey" << endl;
+}
+@end
